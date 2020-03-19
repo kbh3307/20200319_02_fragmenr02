cb2f409b79673f0c6e6b6202b6df6b3c2bb86666
kbh3307 <61855314+kbh3307@users.noreply.github.com>
2020-03-19 12:33:35 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/a20200319_02_fragmenr02/MainActivity.java
@@ -0,0 +1,34 @@
+package kr.co.tjoeun.a20200319_02_fragmenr02;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import kr.co.tjoeun.a20200319_02_fragmenr02.databinding.ActivityMainBinding;
+
+// 프로젝트 기본 세팅
+// Trello => 코드정리 요령 => 3번 내용 따라하기
+
+public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding binding = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
