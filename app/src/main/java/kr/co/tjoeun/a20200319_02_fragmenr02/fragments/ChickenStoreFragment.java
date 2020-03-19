c9865d49cfdf989e7001e26c8e6b29f3819881b2
kbh3307 <61855314+kbh3307@users.noreply.github.com>
2020-03-19 16:06:44 +0900

    프래그먼트 치킨스토어 기본 구성

---
+++ app/src/main/java/kr/co/tjoeun/a20200319_02_fragmenr02/fragments/ChickenStoreFragment.java
@@ -0,0 +1,32 @@
+package kr.co.tjoeun.a20200319_02_fragmenr02.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import kr.co.tjoeun.a20200319_02_fragmenr02.R;
+import kr.co.tjoeun.a20200319_02_fragmenr02.databinding.FragmentChickenStoreBinding;
+
+public class ChickenStoreFragment extends Fragment {
+
+    FragmentChickenStoreBinding binding = null;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_chicken_store, container, false);
+
+                return binding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
